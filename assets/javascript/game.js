f596053c5eb9107de829162db80e05acae3f6d9c
rgao <xinghoshi@gmail.com>
2018-09-28 00:53:25 -0700

    css part 4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -42,7 +42,8 @@ $(document).ready(function() {
         choice5: "Random sub-brown dwarf floating in interstellar space",
         correctChoice: "Pluto",
         correctMsg: "Pluto was demoted to dwarf planet status in 2006 when planets were formally defined as including the requirement of having achieved gravitational independence within its orbit.",
-        incorrectMsg: "The correct choice is 'Pluto.' Silly you, Pluto is not a planet! Pluto was demoted to dwarf planet status in 2006 when planets were formally defined as including the requirement of having achieved gravitational independence within its orbit."
+        incorrectMsg: "The correct choice is 'Pluto.' Silly you, Pluto is not a planet! Pluto was demoted to dwarf planet status in 2006 when planets were formally defined as including the requirement of having achieved gravitational independence within its orbit.",
+        reward: 9,
     };
 
     var q2 = {
@@ -57,6 +58,7 @@ $(document).ready(function() {
         correctChoice: "The Big Bang",
         correctMsg: "The rapid expansion of the universe --of spacetime itself-- at the onset of the Big Bang left a lingering radiation and some uncanny similarity in every direction of the universe.",
         incorrectMsg: "The correct choice is 'The Big Bang.' The CMB can be explained by an event that suddenly caused rapid universal expansion. The uniformity of the CMB, and objects in general, in every direction, including at opposite ends of the universe, imply causality at one point.",
+        reward: 33,
     };
 
     var q3 = {
@@ -71,6 +73,7 @@ $(document).ready(function() {
         correctChoice: "Gravity",
         correctMsg: "The reconciliation of gravity with the other three forces is a difficult one; Grand Unification tends to deal with quantum coupling of the other three forces whereas understanding gravity is usually within the domain of relativity.",
         incorrectMsg: "The correct choice is 'Gravity.' Gravity is the elephant in the room when it comes to fundamental physics. Electromagnetic, Strong, and Weak forces are being understood through quantum mechanics, whereas gravity is understood through relativity.",    
+        reward: 27,
     };
 
     var q4 = {
@@ -85,6 +88,7 @@ $(document).ready(function() {
         correctChoice: "Fat, instead of carbs, is metabolized for energy",
         correctMsg: "Maintaining ketosis is actually a pretty healthy diet practice. Recent research shows that excessive amounts of carbs lead to many health complications whereas fat is not at all unhealthy, like previously thought.",
         incorrectMsg: "The correct choice is 'Fat, instead of carbs, is metabolized for energy.' Ketosis is the process of converting fat into energy; a keto diet consists of high-fat and low-carb foods. By consuming very little amount of carbs, the body seeks fat as a source of energy. This is very effective at losing fat.",    
+        reward: 39,
     };
 
     var q5 = {
@@ -98,6 +102,7 @@ $(document).ready(function() {
         correctChoice: "Electromagnetic force is about 10^33 times stronger",
         correctMsg: "Imagine a small piece of metal and a piece of magnet. The magnet exerts a stronger force on the metal compared to the entirety of Earth!",
         incorrectMsg: "The correct choice is 'Electromagnetic force is about 10^33 times stronger.' Gravity is by far the weakest of the fundamental forces. We are not crushed by Earth's gravity whereas we would die instantly from a stream of loose electricity.",
+        reward: 940,
     };
 
     var q6 = {
@@ -112,6 +117,7 @@ $(document).ready(function() {
         correctChoice: "Pretend to bury nuts",
         correctMsg: "Yup; they pretend to bury nuts to trick observers into thinking there's food but there really isn't. They will then actually bury the nut elsewhere.",
         incorrectMsg: "The correct choice is 'Pretend to bury nuts.' Squirrels are deceptive little beasts; they frequently pretend to be burying nuts to fool observers into searching for nonexistent food.",    
+        reward: 5515,
     };
 
     var q7 = {
@@ -126,6 +132,7 @@ $(document).ready(function() {
         correctChoice: "White Dwarf",
         correctMsg: "The ultimate fate of our Sun is to become a white dwarf after its red giant phase ends in a nova explosion. White dwarves are forever condemned to a dark, cold end as it does not produce nuclear fusion.",
         incorrectMsg: "The correct choice is 'White Dwarf.' Average main sequence stars such as our Sun is primarily supported by gas and radiation pressure. White dwarves are much smaller and denser, relying on electron degeneracy to counteract against its inward gravity.",    
+        reward: 25871,
     };
 
     var q8 = {
@@ -139,7 +146,8 @@ $(document).ready(function() {
         choice5: "It sounds cool and mysterious",
         correctChoice: "It doesn't interact with electromagnetic force",
         correctMsg: "WIMPs (Weakly Interacting Massive Particles) are among the forefront of candidates for dark matter. Dark matter is pretty dense, but unfortunately they evade nearly all electromagnetic detection.",
-        incorrectMsg: "The correct choice is 'It doesn't interact with electromagnetic force.' Dark matter is dark because they do not interact, or interact insignificantly, with the electromagnetic force. We and our instruments rely on electromagnetic detection (including light) to receive any form of information."
+        incorrectMsg: "The correct choice is 'It doesn't interact with electromagnetic force.' Dark matter is dark because they do not interact, or interact insignificantly, with the electromagnetic force. We and our instruments rely on electromagnetic detection (including light) to receive any form of information.",
+        reward: 935972586,
     };
     
     var q9 = {
@@ -154,6 +162,7 @@ $(document).ready(function() {
         correctChoice: "Almond",
         correctMsg: "Squirrels bury nuts to save them for later. For some reason, they tend to bury almonds even if they're not full, presumably because either almonds resemble raw nuts or have high nutritional value for winter.",
         incorrectMsg: "The correct choice is 'Almond.' Peanuts, cashews, and brazil nuts are all loved by squirrels. Raw acorns will be buried, but ripe ones tend to be eaten. For some reason, squirrels sometimes bury almonds even if they're not full.",
+        reward: 144398254969,
     };
 
     var q10 = {
@@ -168,6 +177,7 @@ $(document).ready(function() {
         correctChoice: "'The reason is because Shelton likes selling seashells by the seashore.'",
         correctMsg: "Thank you for not saying and perpetuating this morbidly insulting phrase.",
         incorrectMsg: "The correct choice is 'The reason is because Shelton likes selling seashells by the seashore.' Reason' acts as a noun; a noun can't 'because.' Say 'the reason is that' instead!",
+        reward: 1,
     };
 
     var questionArr = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
@@ -221,15 +231,17 @@ $(document).ready(function() {
         $("#btn-container").show()
         
         if (result === "correct") {
-            score += 100;
+            score += questionArr[questionNum].reward;
             correct++;
             $("#announcement").css("color", "green");
+            $("#announcement").css("background", "radial-gradient(green, white");
             $("#announcement").text("CORRECT!");
             $("#message").text(questionArr[questionNum].correctMsg);
             $("#message").css("color", "green")
 
         } else {
             $("#announcement").css("color", "red");
+            $("#announcement").css("background", "radial-gradient(orangered, white");
             incorrect++;
             if (result === "incorrect") {
                 $("#announcement").text("INCORRECT!");
@@ -250,6 +262,12 @@ $(document).ready(function() {
             $("#final").show();
             $("#time-left-msg").text("Final Result in: ");
 
+            if (correct === questionArr.length) {
+                $("#score").text("$ 1 Vigintillion");
+            } else {
+                $("#score").text("Reward: $"+score);
+            };
+
             impendingDoom = setTimeout(function() {
                 $(".result-screen").hide();
                 $("#time-container").hide();
@@ -258,21 +276,28 @@ $(document).ready(function() {
                 $(".final-screen").show();
                 $("#correct").text("You answered " + correct + " questions correctly.");
                 $("#incorrect").text("You answered " + incorrect + " questions incorrectly.");
-                $("#score").text("Your final reward is $" + score);
 
                 $("#final").hide();
                 $("#start").show();
                 $("#start").text("Play Again!");
+
+                if (correct === questionArr.length) {
+                    $("#final-score").text("You answered all the questions correctly! Congratulations, you're a Vigintillionaire ($10^63)!");
+                } else {
+                    $("#final-score").text("Your final reward is $" + score + ". You did not get all questions correct, so you are not a Vigintillionaire.");
+                };
                     
                 questionNum = 0;
                 correct = 0;
                 incorrect = 0;
                 score = 0;
+                $("#score").text("$0");
             }, 999000);
 
         } else {
             
             $("#time-left-msg").text("Next Question in: ");
+            $("#score").text("Reward: $"+score);
 
             impendingDoom = setTimeout(function() {
                 nextQuestion(questionNum);
@@ -305,15 +330,21 @@ $(document).ready(function() {
         $(".final-screen").show();
         $("#correct").text("You answered " + correct + " questions correctly.");
         $("#incorrect").text("You answered " + incorrect + " questions incorrectly.");
-        $("#score").text("Your final score is " + score);
 
         $("#final").hide();
         $("#start").show();
         $("#start").text("Play Again!");
-                    
+
+        if (correct === questionArr.length) {
+            $("#final-score").text("You answered all the questions correctly! Congratulations, you're a Vigintillionaire ($10^63)!");
+        } else {
+            $("#final-score").text("Your final reward is $" + score + ". You did not get all questions correct, so you are not a Vigintillionaire.");
+        };
+
         questionNum = 0;
         correct = 0;
         incorrect = 0;
         score = 0;
+        $("#score").text("$0");
     });
 })
