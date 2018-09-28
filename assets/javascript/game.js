1b95e15d11f1d7ea85069a4f9a2e155805fcf227
rgao <xinghoshi@gmail.com>
2018-09-27 18:20:40 -0700

    everything except styling?

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     
     // hide main content and show only start button
     $(".main").hide();
+    $(".final-screen").hide();
+    $("#final").hide();
 
     var questionNum = 0;
     var correct = 0;
@@ -15,17 +17,16 @@ $(document).ready(function() {
     // timer object
     var timer = {
 
-        time: 30,
+        time: 45,
         
         reset: function() {
             clearTimeout(impendingDoom);
             clearInterval(display);
-            timer.time = 30;
+            timer.time = 45;
         },
 
         countdown: function() {
             timer.time--;
-            console.log(timer.time);
             $("#time-left").text(timer.time);
         }
     };
@@ -38,24 +39,24 @@ $(document).ready(function() {
         choice2: "Uranus",
         choice3: "Gliese 581 C",
         choice4: "Pluto",
-        choice5: "Random brown dwarf floating in interstellar space",
+        choice5: "Random sub-brown dwarf floating in interstellar space",
         correctChoice: "Pluto",
         correctMsg: "Pluto was demoted to dwarf planet status in 2006 when planets were formally defined as including the requirement of having achieved gravitational independence within its orbit.",
-        incorrectMsg: "Silly you, Pluto is not a planet! Pluto was demoted to dwarf planet status in 2006 when planets were formally defined as including the requirement of having achieved gravitational independence within its orbit."
+        incorrectMsg: "The correct choice is 'Pluto.' Silly you, Pluto is not a planet! Pluto was demoted to dwarf planet status in 2006 when planets were formally defined as including the requirement of having achieved gravitational independence within its orbit."
     };
 
     var q2 = {
 
         number: 2,
-        questionStr: "test question",
-        choice1: "test1",
-        choice2: "test2",
-        choice3: "test3",
-        choice4: "test4",
-        choice5: "test5",
-        correctChoice: "test3",
-        correctMsg: "testmsg1",
-        incorrectMsg: "testmsg2",    
+        questionStr: "The cosmic microwave background (CMB) and its homogeneity and isotropy are strong evidence for which of the following?",
+        choice1: "Dark Matter",
+        choice2: "Quantum Chromodynamics",
+        choice3: "The Big Bang",
+        choice4: "The eventual cosmic 'Crunch'",
+        choice5: "Supermassive black holes",
+        correctChoice: "The Big Bang",
+        correctMsg: "The rapid expansion of the universe --of spacetime itself-- at the onset of the Big Bang left a lingering radiation and some uncanny similarity in every direction of the universe.",
+        incorrectMsg: "The correct choice is 'The Big Bang.' The CMB can be explained by an event that suddenly caused rapid universal expansion. The uniformity of the CMB, and objects in general, in every direction, including at opposite ends of the universe, imply causality at one point.",
     };
 
     var q3 = {
@@ -69,17 +70,116 @@ $(document).ready(function() {
         choice5: "Higgs",
         correctChoice: "Gravity",
         correctMsg: "The reconciliation of gravity with the other three forces is a difficult one; Grand Unification tends to deal with quantum coupling of the other three forces whereas understanding gravity is usually within the domain of relativity.",
-        incorrectMsg: "Gravity is the elephant in the room when it comes to fundamental physics. Electromagnetic, Strong, and Weak forces are being understood through quantum mechanics, whereas gravity is understood through relativity.",    
+        incorrectMsg: "The correct choice is 'Gravity.' Gravity is the elephant in the room when it comes to fundamental physics. Electromagnetic, Strong, and Weak forces are being understood through quantum mechanics, whereas gravity is understood through relativity.",    
     };
 
-    var questionArr = [q1, q2, q3];
+    var q4 = {
+
+        number: 4,
+        questionStr: "What is the idea of the ketosis diet?",
+        choice1: "Limit the amount of fat that your body processes",
+        choice2: "Convert fat into carbs",
+        choice3: "Fat, instead of carbs, is metabolized for energy",
+        choice4: "Convert carbs into fat",
+        choice5: "Metabolize carbs into energy faster",
+        correctChoice: "Fat, instead of carbs, is metabolized for energy",
+        correctMsg: "Maintaining ketosis is actually a pretty healthy diet practice. Recent research shows that excessive amounts of carbs lead to many health complications whereas fat is not at all unhealthy, like previously thought.",
+        incorrectMsg: "The correct choice is 'Fat, instead of carbs, is metabolized for energy.' Ketosis is the process of converting fat into energy; a keto diet consists of high-fat and low-carb foods. By consuming very little amount of carbs, the body seeks fat as a source of energy. This is very effective at losing fat.",    
+    };
+
+    var q5 = {
+        number: 5,
+        questionStr: "What is the strength of gravity compared to electromagnetism?",
+        choice1: "Gravity is about 10 times stronger",
+        choice2: "Gravity is about 10^8 times stronger",
+        choice3: "Electromagnetic force is about 10 times stronger",
+        choice4: "Electromagnetic force is about 10^33 times stronger",
+        choice5: "They're about the same in strength",
+        correctChoice: "Electromagnetic force is about 10^33 times stronger",
+        correctMsg: "Imagine a small piece of metal and a piece of magnet. The magnet exerts a stronger force on the metal compared to the entirety of Earth!",
+        incorrectMsg: "The correct choice is 'Electromagnetic force is about 10^33 times stronger.' Gravity is by far the weakest of the fundamental forces. We are not crushed by Earth's gravity whereas we would die instantly from a stream of loose electricity.",
+    };
+
+    var q6 = {
+
+        number: 6,
+        questionStr: "Standard fox and gray squirrels employ which of the following tactics to attempt to protect their food?",
+        choice1: "Store food deep underground",
+        choice2: "Hide them in their cheeks",
+        choice3: "Stack nuts in their drey",
+        choice4: "Keep poisonous nuts to deter thieves",
+        choice5: "Pretend to bury nuts",
+        correctChoice: "Pretend to bury nuts",
+        correctMsg: "Yup; they pretend to bury nuts to trick observers into thinking there's food but there really isn't. They will then actually bury the nut elsewhere.",
+        incorrectMsg: "The correct choice is 'Pretend to bury nuts.' Squirrels are deceptive little beasts; they frequently pretend to be burying nuts to fool observers into searching for nonexistent food.",    
+    };
+
+    var q7 = {
+
+        number: 7,
+        questionStr: "Electron degeneracy pressure is the primary support in hydrostatic equilibrium for which of the following astronomical objects?",
+        choice1: "Average main sequence stars, such as the Sun",
+        choice2: "Red Supergiant",
+        choice3: "White Dwarf",
+        choice4: "Pulsars",
+        choice5: "Active Galactic Nuclei",
+        correctChoice: "White Dwarf",
+        correctMsg: "The ultimate fate of our Sun is to become a white dwarf after its red giant phase ends in a nova explosion. White dwarves are forever condemned to a dark, cold end as it does not produce nuclear fusion.",
+        incorrectMsg: "The correct choice is 'White Dwarf.' Average main sequence stars such as our Sun is primarily supported by gas and radiation pressure. White dwarves are much smaller and denser, relying on electron degeneracy to counteract against its inward gravity.",    
+    };
+
+    var q8 = {
+
+        number: 8,
+        questionStr: "Why is dark matter known as such?",
+        choice1: "It is dark in color",
+        choice2: "It doesn't interact with electromagnetic force",
+        choice3: "They're gravitons",
+        choice4: "They produce only gamma and cosmic radiation",
+        choice5: "It sounds cool and mysterious",
+        correctChoice: "It doesn't interact with electromagnetic force",
+        correctMsg: "WIMPs (Weakly Interacting Massive Particles) are among the forefront of candidates for dark matter. Dark matter is pretty dense, but unfortunately they evade nearly all electromagnetic detection.",
+        incorrectMsg: "The correct choice is 'It doesn't interact with electromagnetic force.' Dark matter is dark because they do not interact, or interact insignificantly, with the electromagnetic force. We and our instruments rely on electromagnetic detection (including light) to receive any form of information."
+    };
+    
+    var q9 = {
+
+        number: 9,
+        questionStr: "Of the following nuts, which is most likely to be buried (as opposed to eaten immediately) by a friendly fox squirrel?",
+        choice1: "Almond",
+        choice2: "Brazil Nut",
+        choice3: "Cashew",
+        choice4: "Peanut",
+        choice5: "Ripe Acorn",
+        correctChoice: "Almond",
+        correctMsg: "Squirrels bury nuts to save them for later. For some reason, they tend to bury almonds even if they're not full, presumably because either almonds resemble raw nuts or have high nutritional value for winter.",
+        incorrectMsg: "The correct choice is 'Almond.' Peanuts, cashews, and brazil nuts are all loved by squirrels. Raw acorns will be buried, but ripe ones tend to be eaten. For some reason, squirrels sometimes bury almonds even if they're not full.",
+    };
+
+    var q10 = {
+
+        number: 10,
+        questionStr: "Which of the following phrases is grammatically incorrect?",
+        choice1: "'It's just that that person likes selling seashells by the seashore.",
+        choice2: "'The reason is because Shelton likes selling seashells by the seashore.'",
+        choice3: "'This is literally the easiest selling of seashells by the seashore ever.'",
+        choice4: "'Shelton has been selling seashells by the seething seashore.'",
+        choice5: "'This is a better idea than his selling seashells by the seashore.'",
+        correctChoice: "'The reason is because Shelton likes selling seashells by the seashore.'",
+        correctMsg: "Thank you for not saying and perpetuating this morbidly insulting phrase.",
+        incorrectMsg: "The correct choice is 'The reason is because Shelton likes selling seashells by the seashore.' Reason' acts as a noun; a noun can't 'because.' Say 'the reason is that' instead!",
+    };
+
+    var questionArr = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
 
     // when start button clicked, hide start button and starts with 1st question
     $("#start").on("click", function() {
 
-        $("#start").hide();
+        $("#btn-container").hide();
+        $(".final-screen").hide();
         $(".main").show();
         $("#time-container").show();
+        $("#q-container").show()
         nextQuestion(questionNum);
     });
 
@@ -90,6 +190,7 @@ $(document).ready(function() {
         $(".result-screen").hide();
 
         timer.reset();
+        timer.time = timer.time + 5 * questionNum;
         $("#time-left-msg").text("Time Remaining: ");
         $("#time-left").text(timer.time);
 
@@ -101,7 +202,6 @@ $(document).ready(function() {
         $("#choice4").html(questionArr[questionNum].choice4);
         $("#choice5").html(questionArr[questionNum].choice5);
 
-        // timer.time = timer.time + 5 * questionNum;
         impendingDoom = setTimeout(resultScreen, timer.time * 1000, "timeup");
         display = setInterval(timer.countdown, 1000);
     };
@@ -110,18 +210,22 @@ $(document).ready(function() {
     var resultScreen = function(result) {
 
         timer.reset();
-        timer.time = 10;
-        $("#time-left-msg").text("Next Question in: ");
+        timer.time = 159;
         $("#time-left").text(timer.time);
+        display = setInterval(timer.countdown, 1000);
 
         $(".main-screen").hide();
         $(".result-screen").show();
+        $("#btn-container").show()
         
         if (result === "correct") {
+            score += 100;
+            correct++;
             $("#announcement").text("CORRECT!");
             $("#message").text(questionArr[questionNum].correctMsg);
         
         } else {
+            incorrect++;
             if (result === "incorrect") {
                 $("#announcement").text("INCORRECT!");
             } else {
@@ -130,28 +234,44 @@ $(document).ready(function() {
             $("#message").text(questionArr[questionNum].incorrectMsg);
         };
 
-        display = setInterval(timer.countdown, 1000);
-
+        // increment question number
         questionNum++
+
+        // when all questions are asked
         if (questionNum === questionArr.length) {
 
-            $("#time-container").hide()
-            timer.reset();
+            $("#start").hide();
+            $("#final").show();
+            $("#time-left-msg").text("Final Result in: ");
 
             impendingDoom = setTimeout(function() {
-                $(".main").hide();
+                $(".result-screen").hide();
+                $("#time-container").hide();
+                $("#q-container").hide();
+
+                $(".final-screen").show();
+                $("#correct").text("You answered " + correct + " questions correctly.");
+                $("#incorrect").text("You answered " + incorrect + " questions incorrectly.");
+                $("#score").text("Your final score is " + score);
+
+                $("#final").hide();
                 $("#start").show();
                 $("#start").text("Play Again!");
+                    
                 questionNum = 0;
                 correct = 0;
                 incorrect = 0;
                 score = 0;
-            }, 10000);
+            }, 159000);
 
         } else {
+            
+            $("#time-left-msg").text("Next Question in: ");
+
             impendingDoom = setTimeout(function() {
                 nextQuestion(questionNum);
-            }, 10000);
+            }, 159000);
+            $("#start").text("Next Question");
         };
     };
 
@@ -162,13 +282,32 @@ $(document).ready(function() {
         var answer = $("span", this).text();
 
         if (answer === questionArr[questionNum].correctChoice) {
-            score += 100;
-            correct++;
             resultScreen("correct");
 
         } else {
-            incorrect++;
             resultScreen("incorrect");
         };
+    });
+
+    // displays final result screen when final result button is clicked
+    $("#final").on("click", function() {
+
+        $(".result-screen").hide();
+        $("#time-container").hide();
+        $("#q-container").hide();
+
+        $(".final-screen").show();
+        $("#correct").text("You answered " + correct + " questions correctly.");
+        $("#incorrect").text("You answered " + incorrect + " questions incorrectly.");
+        $("#score").text("Your final score is " + score);
+
+        $("#final").hide();
+        $("#start").show();
+        $("#start").text("Play Again!");
+                    
+        questionNum = 0;
+        correct = 0;
+        incorrect = 0;
+        score = 0;
     });
 })
